3b44a2360a60f2ea0290b2fd0f7153ab0742077d
Roshan Thomas <roshanpattathilsep16@gmail.com>
2026-01-23 23:55:14 +0530

    Clear OpenWeather API key in config

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,6 +1,7 @@
 import os
 
 # Put your real API key here
-OPENWEATHER_API_KEY = "4fcfc437d73dd09ac129306e30d0784e"
+OPENWEATHER_API_KEY = ""
 
 BASE_URL = "https://api.openweathermap.org/data/2.5"
+
